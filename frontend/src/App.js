cc171fd2f1dc64a6e4c54937859fc2a92cd3b200
C4Q <c4q@C4Qs-MBP-80.home>
2018-05-10 10:10:54 -0400

    installed create-react-app inside frontend folder and axios react-router-dom dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link, Switch } from 'react-router-dom';
+import Register from './components/Register';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Register} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
